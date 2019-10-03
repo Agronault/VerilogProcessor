db20f6a51d277669da0e3fb63f8df78d33dd6269
Thiago Dornelles <thiago.fdornelles@gmail.com>
2019-10-03 07:59:59 -0300

    starting implementation fro fpga

--- processor.v
+++ processor.v
@@ -1,4 +1,4 @@
-module processor(SW, Clock, Done, BusWires);
+module processor(SW, Clock, Done, BusWires, HEX0, HEX1, HEX4, HEX5, HEX6, HEX7);
 	
 	input [17:0]SW;// 0-15 DIN, 16 Resetn, 17 Run
 	input Clock;
@@ -8,7 +8,9 @@ module processor(SW, Clock, Done, BusWires);
 	
 	reg[15:0] addr, memData;
 	reg save;
-  initial
+	output [6:0] HEX0, HEX1, HEX6, HEX7, HEX4, HEX5;
+
+        initial
 	begin
 	  save = 1'b0;
 		mem[3] = 16'b0000000000000100;
@@ -26,4 +28,13 @@ module processor(SW, Clock, Done, BusWires);
 		  mem[addr] = BusWires;
 	end
 	
+	hexto7segment inst(SW[3:0], HEX0);
+
+	hexto7segment regX({1'b0, SW[6:4]}, HEX1);
+	hexto7segment regY({1'b0, SW[9:7]}, HEX4);
+
+	//hexto7segment din1(SW[7:4], HEX5);
+	//hexto7segment din2(SW[14:11], HEX6);
+	//hexto7segment din3(SW[15], HEX7);
+
 endmodule
